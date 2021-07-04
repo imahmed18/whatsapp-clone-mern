ca4d06235848f8987450a1e6c1dfc941bdd11e17
Ahmed Raza <imahmed18@gmail.com>
2021-07-04 19:06:18 +0500

    final

---
+++ whatsapp-backend/server.js
@@ -0,0 +1,86 @@
+//password: LKi7BtQ4soI5kxZh
+
+// importing
+import express from "express";
+import mongoose from "mongoose";
+import Messages from "./dbMessages.js";
+import Pusher from "pusher";
+import cors from "cors";
+
+//app config
+const app = express();
+const port = process.env.PORT || 9000;
+
+const pusher = new Pusher({
+    appId: "1229442",
+    key: "6e6157a8e86479645847",
+    secret: "7eaf50de6f516f5f91a5",
+    cluster: "mt1",
+    useTLS: true
+  });
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// DB config
+const connection_url =
+  "mongodb+srv://admin:LKi7BtQ4soI5kxZh@cluster0.nwxia.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(connection_url, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// pusher
+const db = mongoose.connection;
+
+db.once("open", () => {
+  console.log("DB connected");
+
+  const msgCollection = db.collection("messagecontents");
+  const changeStream = msgCollection.watch();
+
+  changeStream.on("change", (change) => {
+    console.log("A change occured", change);
+
+    if (change.operationType === "insert") {
+      const messageDetails = change.fullDocument;
+      pusher.trigger("message", "inserted", {
+        name: messageDetails.name,
+        message: messageDetails.message,
+        timestamp: messageDetails.timestamp,
+        received: messageDetails.received,
+      });
+    } else {
+      console.log("Error triggering Pusher");
+    }
+  });
+});
+
+// api routes
+app.get("/", (req, res) => res.status(200).send("hello world"));
+
+app.get("/messages/sync", (req, res) => {
+  Messages.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.post("/messages/new", (req, res) => {
+  const dbMessage = req.body;
+  Messages.create(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+// listen
+app.listen(port, () => console.log(`Listening on localhost:${port}`));
---
+++ whatsapp-clone/src/App.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Sidebar from "./Sidebar";
+import Chat from "./Chat";
+import Pusher from "pusher-js";
+import axios from "./axios";
+
+function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    axios.get("/messages/sync").then((response) => {
+      setMessages(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    const pusher = new Pusher('6e6157a8e86479645847', {
+      cluster: 'mt1'
+    });
+
+    const channel = pusher.subscribe('message');
+    channel.bind('inserted', (newMessage) => {
+      setMessages([...messages, newMessage]);
+    });
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [messages]);
+
+  console.log(messages);
+
+  return (
+    <div className="app">
+      <div className="app__body">
+        <Sidebar />
+        <Chat messages={messages} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
